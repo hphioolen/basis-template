a8766be1a8d7a6ddd6302960a31fc814ac649906
hphioolen <hphioolen@MacBook-Pro-van-Hans-Peter-2.local>
2015-10-28 16:32:46 +0100

    Footer toegevoegd

---
+++ lib/setup.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Register sidebars
+ */
+function widgets_init() {
+  register_sidebar([
+    'name'          => __('Primary', 'sage'),
+    'id'            => 'sidebar-primary',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
+
+register_sidebar(array(
+    'name'          => __('Footer Links', 'sage'),
+    'id'            => 'sidebar-footer-1',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+  
+   register_sidebar(array(
+    'name'          => __('Footer Midden', 'sage'),
+    'id'            => 'sidebar-footer-2',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+  
+   register_sidebar(array(
+    'name'          => __('Footer Rechts', 'sage'),
+    'id'            => 'sidebar-footer-3',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+
+}
+add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
